a3b4bfd0ce16c17e9dc93b81df7d80c2b6e1ec15
Valera Rozuvan <valera@rozuvan.net>
2026-05-10 07:20:51 -0400

    stop git tracking build folder of the pty node wrapper

--- ui-tests/pty/build/Release/.deps/Release/pty_native.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/pty_native.node := ln -f "Release/obj.target/pty_native.node" "Release/pty_native.node" 2>/dev/null || (rm -rf "Release/pty_native.node" && cp -af "Release/obj.target/pty_native.node" "Release/pty_native.node")
